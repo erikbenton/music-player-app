f06467335be3eaec30e04e76c0082700fc6bf89a
erikbenton <erik.benton@hotmail.com>
2018-03-30 13:49:50 -0400

    adding onCompletionListener

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mp = MediaPlayer.create(this, R.raw.the_magic);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast toast=Toast.makeText(getApplicationContext(),"I'm done!",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
         Button playButton  = (Button)findViewById(R.id.play_button);
         Button pauseButton = (Button)findViewById(R.id.pause_button);
